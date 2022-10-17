f8e0351c830d0b340ad2d718e82ece2a79de20c3
William Lines <williamlines42@gmail.com>
2022-10-17 11:39:45 +0100

    updated task_list_spec.rb to fully test the TaskList class

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -11,5 +11,30 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
+  it "returns tasks with all" do
+    task_list = TaskList.new
+    task_1 = double(:task_1)
+    task_2 = double(:task_2)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+  
+  it "can return false if not all tasks complete with .all_complete" do
+    task_list = TaskList.new
+    task_1 = double(:task_1, complete?: false)
+    task_2 = double(:task_2, complete?: true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to be false
+  end
+  it "can return true if all tasks complete with .all_complete" do
+    task_list = TaskList.new
+    task_1 = double(:task_1, complete?: true)
+    task_2 = double(:task_2, complete?: true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to be true
+  end
   # Unit test `#all` and `#all_complete?` behaviour
 end
